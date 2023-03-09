c0156c1759945effc0be54a6181e1db68ded12b2
Juan Pablo Portillo <juanp.portillob@gmail.com>
2023-03-09 11:12:28 -0600

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # List all restaurants
+  get "restaurants", to: "restaurants#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "restaurants/new", to: "restaurants#new"
+
+  post "restaurants", to: "restaurants#create"
+
+
+  get "restaurants/:id", to: "restaurants#show", as: "restaurant"
+
+  get "restaurants/:restaurant_id/reviews/new", to: "reviews#new", as: "new_restaurant_review"
+
+
+  post "restaurants/:restaurant_id/reviews", to: "reviews#create", as: "restaurant_reviews"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Review.destroy_all
+Restaurant.destroy_all
+
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+
+  3.times do
+    review = Review.create!(
+      content: Faker::Lorem.sentence,
+      rating: rand(0..5),
+      restaurant: restaurant
+    )
+  end
+end
